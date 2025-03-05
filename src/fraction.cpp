a8fb4147393d822e5d9f6edfba3d33fb3cc93a38
PaulineCosson <cosson.pa@gmail.com>
2025-03-05 12:34:03 +0100

    Exo 1 à 4

--- src/fraction.cpp
+++ src/fraction.cpp
@@ -1,13 +1,19 @@
 #include "fraction.hpp"
 #include "utils.hpp"
 
-#include <iostream>
 
-void Fraction::display() {
-    std::cout << numerator << "/" << denominator;
+//Transformer la fonction display en surcharge de l'opérateur << 
+//pour pouvoir afficher une fraction avec std::cout (opérateurs de flux).
+
+// void Fraction::display() {
+//     std::cout << numerator << "/" << denominator;
+// }
+
+std::ostream& operator<<(std::ostream& os, Fraction const& f) {
+    return os << f.numerator << "/" << f.denominator;
 }
 
-Fraction add(Fraction const& f1, Fraction const& f2) {
+Fraction operator+(Fraction const& f1, Fraction const& f2) {
     return simplify({
         f1.numerator * f2.denominator + f2.numerator * f1.denominator,
         f1.denominator * f2.denominator
@@ -24,23 +30,56 @@ Fraction add(Fraction const& f1, Fraction const& f2) {
 // }
 
 
-Fraction sub(Fraction const& f1, Fraction const& f2) {
+Fraction operator-(Fraction const& f1, Fraction const& f2) {
     return simplify({
         f1.numerator * f2.denominator - f2.numerator * f1.denominator,
         f1.denominator * f2.denominator
     });
 }
 
-Fraction mul(Fraction const& f1, Fraction const& f2) {
+Fraction operator*(Fraction const& f1, Fraction const& f2) {
     return simplify({
         f1.numerator * f2.numerator,
         f1.denominator * f2.denominator
     });
 }
 
-Fraction div(Fraction const& f1, Fraction const& f2) {
+Fraction operator/(Fraction const& f1, Fraction const& f2) {
     return simplify({
         f1.numerator * f2.denominator,
         f1.denominator * f2.numerator
     });
+}
+
+bool operator==(Fraction const& f1, Fraction const& f2) {
+    return (f1.numerator * f2.denominator == f1.denominator * f2.numerator);
+}
+
+bool operator!=(Fraction const& f1, Fraction const& f2) {
+    return !(f1 == f2);
+}
+
+//exo 4
+bool operator<(Fraction const& f1, Fraction const& f2) {
+    return (f1.numerator / f2.denominator < f1.denominator / f2.numerator);
+}
+
+bool operator<=(Fraction const& f1, Fraction const& f2) {
+    return (f1<f2 || f1==f2);
+}
+
+bool operator>(Fraction const& f1, Fraction const& f2) {
+    return !(f1<=f2);
+}
+
+bool operator>=(Fraction const& f1, Fraction const& f2) {
+    return !(f1<f2);
+}
+
+//exo 5
+Fraction& Fraction::operator+=(Fraction const& f) {
+    numerator = numerator * f.denominator + f.numerator * denominator;
+    denominator = denominator * f.denominator;
+    *this = simplify(*this);
+    return *this;
 }
--- src/fraction.hpp
+++ src/fraction.hpp
@@ -1,13 +1,25 @@
 #pragma once
+#include <iostream>
+
 
 struct Fraction {
     unsigned int numerator;
     unsigned int denominator;
 
-    void display();
+    // void display();
+    Fraction& Fraction::operator+=(Fraction const& f);
 };
 
-Fraction add(Fraction const& f1, Fraction const& f2);
-Fraction sub(Fraction const& f1, Fraction const& f2);
-Fraction mul(Fraction const& f1, Fraction const& f2);
-Fraction div(Fraction const& f1, Fraction const& f2);
+std::ostream& operator<<(std::ostream& os, Fraction const& f);
+Fraction operator+(Fraction const& f1, Fraction const& f2);
+Fraction operator-(Fraction const& f1, Fraction const& f2);
+Fraction operator*(Fraction const& f1, Fraction const& f2);
+Fraction operator/(Fraction const& f1, Fraction const& f2);
+
+bool operator==(Fraction const& f1, Fraction const& f2);
+bool operator!=(Fraction const& f1, Fraction const& f2);
+
+bool operator<(Fraction const& f1, Fraction const& f2);
+bool operator<=(Fraction const& f1, Fraction const& f2);
+bool operator>(Fraction const& f1, Fraction const& f2);
+bool operator>=(Fraction const& f1, Fraction const& f2);
